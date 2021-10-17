02886d99da5f38013c6247b4614247a7ebe6dd49
Momijiichigo <ichigomomiji436@gmail.com>
2021-10-17 00:42:52 -0700

    hmr option

--- vite-with-bluejsx/src/index.ts
+++ vite-with-bluejsx/src/index.ts
@@ -2,15 +2,20 @@ import HMRLoader from '@bluejsx/vite-plugin-blue-hmr'
 import mdxLoader from '@bluejsx/vite-plugin-bluemdx'
 import { UserConfig } from 'vite'
 
-export default function withBlueJSX(config: UserConfig): UserConfig {
+export default function withBlueJSX(config: UserConfig & {
+  bluejsx?: {
+    hmr?: boolean
+  }
+}): UserConfig {
   config.esbuild ??= {}
   const esbuild = config.esbuild
   esbuild.jsxFactory = 'Blue.r'
   esbuild.jsxFragment = 'Blue.Fragment'
   esbuild.jsxInject = `import Blue from 'bluejsx'`
 
+  const useHMR = config.bluejsx?.hmr ?? true
   config.plugins ??= []
-  config.plugins.push(HMRLoader(), mdxLoader())
+  config.plugins.push(HMRLoader({enabled: useHMR}), mdxLoader())
 
   
   return config
