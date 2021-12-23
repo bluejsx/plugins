1bf3f80ea812505af43c4506acc25bf58a69aa58
Momijiichigo <ichigomomiji436@gmail.com>
2021-12-22 20:36:52 -0800

    fix option type

--- vite-bluejsx-with-pages/lib/index.d.ts
+++ vite-bluejsx-with-pages/lib/index.d.ts
@@ -1,5 +1,6 @@
 import { UserConfig } from 'vite';
+import withBlueJSX from 'vite-with-bluejsx';
 
-declare function withPages(config: UserConfig): UserConfig;
+declare function withPages(config: Parameters<typeof withBlueJSX>[0]): UserConfig;
 
 export { withPages as default };
